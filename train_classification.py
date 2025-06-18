a889083f689e751eb9f2feaabf4f6ddadb7ba317
jisnoo123 <jisnoodevpal@gmail.com>
2025-06-19 01:29:30 +0530

    WTF Kees

--- train_classification.py
+++ train_classification.py
@@ -167,6 +167,7 @@ def main(args):
     best_class_acc = 0.0
 
     '''TRANING'''
+    
     logger.info('Start training...')
     for epoch in range(start_epoch, args.epoch):
         log_string('Epoch %d (%d/%s):' % (global_epoch + 1, epoch + 1, args.epoch))
